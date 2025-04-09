5b18af3239dd35479c44802dde8bff2c78eab2c5
m.kwgch <m.kwgch@gmail.com>
2025-04-09 16:56:15 +0900

    Implement translation bot with language selection and chat functionality

--- src/016/js/views/ChatView.js
+++ src/016/js/views/ChatView.js
@@ -1,19 +1,24 @@
 class ChatView {
     constructor(containerId) {
         this.container = document.getElementById(containerId);
+        this.chatArea = document.getElementById('chat-area');
     }
 
     render(messages) {
-        this.container.innerHTML = '';
+        this.chatArea.innerHTML = '';
         messages.forEach(message => {
             const messageElement = document.createElement('div');
             messageElement.classList.add('message');
             messageElement.classList.add(message.sender);
             messageElement.innerHTML = `
-                ${message.sender === 'user' ? '<i class="fas fa-face-smile"></i>' : '<i class="fas fa-robot"></i>'}
-                <span>${message.text}</span>
+                <div class="avatar">
+                    ${message.sender === 'user' ? '<i class="fas fa-face-smile"></i>' : '<i class="fas fa-robot"></i>'}
+                </div>
+                <div class="message-text">
+                    <span>${message.text}</span>
+                </div>
             `;
-            this.container.appendChild(messageElement);
+            this.chatArea.appendChild(messageElement);
         });
     }
 }
